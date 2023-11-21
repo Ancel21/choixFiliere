82677d186c809a4de9aca2cc0a5218c42bc4ca84
ANCEL YOUNG NKAMNGANG TCHANA <ancelyoung@yahoo.fr>
2023-11-21 02:14:36 +0100

    Ceci est la première version fonctionnelle du projet

--- main.py
+++ main.py
@@ -86,50 +86,12 @@ weights = {
     "Ambitions & objectifs professionnels": 1.0
 }
 
-# Itération à travers les lignes du DataFrame
-for index, row in data.iterrows():
-    student_id = row.iloc[0]  # Obtenez le numéro étudiant de la 1ère colonne
 
-    # Initialisation des totaux de points pour IPS et ASTRE pour chaque étudiant
-    total_ips = 0
-    total_astre = 0
-
-    # Colonnes pour le critère "Techno & outils"
-    techno_columns = [1, 2, 3]  # Remplacez ces numéros par les indices corrects de vos colonnes
-
-    # Calcul des scores en fonction des réponses de l'étudiant et des poids des critères
-    for col_idx in techno_columns:
-        response = row.iloc[col_idx]
-
-        # Critère "Techno & outils"
-        total_ips += mots_cles_ips.get(response, 0) * weights.get("Techno & outils", 1.0)
-        total_astre += mots_cles_astre.get(response, 0) * weights.get("Techno & outils", 1.0)
-
-    # Ajoutez d'autres conditions pour les autres critères
-
-    # Vérification du choix en fonction des totaux de points
-    if total_ips > total_astre:
-        choix_filiere = "IPS"
-    elif total_astre > total_ips:
-        choix_filiere = "ASTRE"
-    else:
-        choix_filiere = "Indécis"
-
-    # Stockez le choix de filière dans le dictionnaire
-    choices[student_id] = choix_filiere
-
-# Comptez le nombre d'étudiants pour chaque filière
-count_ips = sum(1 for choix in choices.values() if choix == "IPS")
-count_astre = sum(1 for choix in choices.values() if choix == "ASTRE")
-count_indecis = sum(1 for choix in choices.values() if choix == "Indécis")
-
-@app.route('/')
-def index():
-    return render_template('index.html', weights=weights)
-
-@app.route('/result', methods=['POST'])
-def result():
+# Fonction pour calculer le score total
+def calculate_total_score():
     global weights
+    global choices
+
     # Récupérer les poids des critères depuis le formulaire
     weights = {
         "Techno & outils": float(request.form.get('weight_techno')),
@@ -139,7 +101,7 @@ def result():
 
     # Itération à travers les lignes du DataFrame
     for index, row in data.iterrows():
-        student_id = row.iloc[0]  # Obtenez le numéro étudiant de la 1ère colonne
+        student_id = row.iloc[1]  # Obtenez le numéro étudiant de la 1ère colonne
 
         # Initialisation des totaux de points pour IPS et ASTRE pour chaque étudiant
         total_ips = 0
@@ -147,7 +109,7 @@ def result():
 
         # Colonnes pour le critère "Techno & outils"
         techno_columns = [2, 3, 4]  # Remplacez ces numéros par les indices corrects de vos colonnes
-        passion_columns = [5, 6, 7,17]
+        passion_columns = [5, 6, 7, 17]
         objectifs_columns = [12, 13, 14, 15]
 
         # Calcul des scores en fonction des réponses de l'étudiant et des poids des critères
@@ -183,14 +145,38 @@ def result():
             choix_filiere = "Indécis"
 
         # Stockez le choix de filière dans le dictionnaire
-        choices[student_id] = choix_filiere
+        choices[student_id] = {"choix_filiere": choix_filiere, "total_astre": total_astre, "total_ips": total_ips}
 
+    return choices
+
+# Route principale
+@app.route('/')
+def index():
+    return render_template('index.html', weights=weights)
+
+
+# Route pour afficher les résultats
+@app.route('/result', methods=['POST'])
+def result():
+    calculate_total_score()
     # Comptez le nombre d'étudiants pour chaque filière
-    count_ips = sum(1 for choix in choices.values() if choix == "IPS")
-    count_astre = sum(1 for choix in choices.values() if choix == "ASTRE")
-    count_indecis = sum(1 for choix in choices.values() if choix == "Indécis")
+    count_ips = sum(1 for choix in choices.values() if choix["choix_filiere"] == "IPS")
+    count_astre = sum(1 for choix in choices.values() if choix["choix_filiere"] == "ASTRE")
+    count_indecis = sum(1 for choix in choices.values() if choix["choix_filiere"] == "Indécis")
+    return render_template('result.html', count_ips=count_ips, count_astre=count_astre, count_indecis=count_indecis,weights=weights, choix=choices)
 
-    return render_template('result.html', count_ips=count_ips, count_astre=count_astre, count_indecis=count_indecis, weights=weights)
+
+@app.route('/student_detail', methods=['POST'])
+def student_detail():
+    student_id = request.form.get('student_id')
+
+    # Vérifiez si l'étudiant existe dans choices
+    if student_id in choices:
+        # Récupérez le choix de filière pour cet étudiant
+        student_choice = choices[student_id]
+
+        return render_template('student_detail.html', student_id=student_id, student_choice=student_choice, data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
